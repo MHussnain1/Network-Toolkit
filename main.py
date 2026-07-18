10512e83bf69c0c9a5669a74ac7e9071b90b8a3e
MHussnain1 <alihusnan565@gmail.com>
2026-07-18 08:10:50 +0500

    main integration

--- main.py
+++ main.py
@@ -11,22 +11,30 @@ def main():
 
         print("1. Hostname Resolver")
         print("2. Port Scanner")
-        print("3. Ping host")
-        print("4. Exit")
+        print("3. Ping Host")
+        print("4. Local IP")
+        print("5. Exit")
 
-        choice = input("\nEnter your choice (1-4): ").strip()
+        choice = input("\nEnter your choice (1-5): ").strip()
 
         if choice == '1':
             hostname_resolver()
+
         elif choice == '2':
             port_scanner()
+
         elif choice == '3':
             controller()
+
         elif choice == '4':
+            print("\nLocal IP coming soon.")
+
+        elif choice == '5':
             print("\nExiting the program. Goodbye!")
             break
+
         else:
-            print("\nInvalid choice. Please enter a number between 1 and 4.")
+            print("\nInvalid choice. Please enter a number between 1 and 5.")
 
 
 if __name__ == "__main__":
