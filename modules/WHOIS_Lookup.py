3a88cbd93335cc7b8ff1a222f9273cda88759ea5
MHussnain1 <alihusnan565@gmail.com>
2026-07-27 12:50:20 +0500

    added dispaly f()

--- modules/WHOIS_Lookup.py
+++ modules/WHOIS_Lookup.py
@@ -13,10 +13,23 @@ def whois_info(domain):
     result = whois.whois(domain)
     return result
 
+def display(result):
+    print("\n---------------------------")
+    print("\n==============================")
+    print("Results")
+    print("==============================")
+    domain_name = result.get("domain_name")
+    registrar = result.get("registrar")
+    country = result.get("country")
+    creation_date = result.get("creation_date")[0] if result.get("creation_date") else "Not available"
+    expiration_date = result.get("expiration_date")[0] if result.get("expiration_date") else "Not available"
+    updated_date = result.get("updated_date")[0] if result.get("updated_date") else "Note Available"
+
+    print(f"\n domain_name: '{domain_name}' \n registrar : '{registrar}' \n country : '{country}' \n creation date : '{creation_date}'\n expiration date : '{expiration_date}'\n update date : {updated_date}")
 
 
 
 def controller():
     domain_name = get_domain()
     result = whois_info(domain_name)
-    print(result)
+    display(result)
