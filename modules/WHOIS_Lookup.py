0eb6522611b73afac4741bcd7d461502adf82b55
MHussnain1 <alihusnan565@gmail.com>
2026-07-27 18:42:05 +0500

    intigrated to main

--- modules/WHOIS_Lookup.py
+++ modules/WHOIS_Lookup.py
@@ -2,31 +2,43 @@ import whois
 
 
 def get_domain():
-    print("="*16)
-    print("WHOIS_LookUp" )
-    print("="*16)
+    print("=" * 16)
+    print("WHOIS_LookUp")
+    print("=" * 16)
 
     domain = input("Enter domain: ").strip()
     return domain
 
+
 def whois_info(domain):
     result = whois.whois(domain)
     return result
+
+
+def get_first(value):
+    if isinstance(value, list):
+        return value[0] if value else "Not available"
+    return value if value else "Not available"
+
 
 def display(result):
     print("\n---------------------------")
     print("\n==============================")
     print("Results")
     print("==============================")
+
     domain_name = result.get("domain_name")
     registrar = result.get("registrar")
     country = result.get("country")
-    creation_date = result.get("creation_date")[0] if result.get("creation_date") else "Not available"
-    expiration_date = result.get("expiration_date")[0] if result.get("expiration_date") else "Not available"
-    updated_date = result.get("updated_date")[0] if result.get("updated_date") else "Note Available"
+    organization = result.get("org")
+    name_servers = result.get("name_servers")
+    creation_date = get_first(result.get("creation_date"))
+    expiration_date = get_first(result.get("expiration_date"))
+    updated_date = get_first(result.get("updated_date"))
 
-    print(f"\n domain_name: '{domain_name}' \n registrar : '{registrar}' \n country : '{country}' \n creation date : '{creation_date}'\n expiration date : '{expiration_date}'\n update date : {updated_date}")
-
+    print(
+        f"Domain Name     : {domain_name}\nRegistrar       : {registrar}\nOrganization    : {organization}\nCountry         : {country}\nServer Name     : {', '.join(name_servers)}\nCreation Date   : {creation_date}\nExpiration Date : {expiration_date}\nUpdated Date    : {updated_date}"
+    )
 
 
 def controller():
